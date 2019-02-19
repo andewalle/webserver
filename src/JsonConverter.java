6b0af461f64958916495f2d32131c950a78c69aa
Simon Seabird <m13simon@gmail.com>
2019-02-19 16:02:12 +0000

    Boom

--- src/JsonConverter.java
+++ src/JsonConverter.java
@@ -5,25 +5,7 @@ import java.io.*;
 public class JsonConverter {
     private Gson gson = new Gson();
 
-    //Returns a Json string of a object.
-    public String objectToJsonString(Object obj){
-        return gson.toJson(obj);
-    }
-
-    //File("path to where you want to save the json file")
-    public void objectToJsonFile(Object obj, File f) throws IOException {
-        FileWriter fw = new FileWriter(f);
-        gson.toJson(obj, fw);
-        fw.close();
-    }
-
-    //Convert a json string to a object.
-    public Object jsonStringToObject(String s){
-        return gson.fromJson(s, Object.class);
-    }
-
-    //File("path to the file you want to make into a object")
-    public Object jsonFileToObject(File f) throws FileNotFoundException {
-        return gson.fromJson(new FileReader(f), Object.class);
+    public JsonConverter(Person p){
+        gson.toJson(p);
     }
 }
